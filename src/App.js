49a9eaa067346afad5a251baaa2b50fd4569b731
Jonathan Melnik <Jonathanmelnik@gmail.com>
2022-01-16 14:11:46 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
+const ethers = require("ethers");
+
+const { ethereum } = window;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [signer, setSigner] = useState(null);
+    const [address, setAddress] = useState(null);
+    const [contract, setContract] = useState(null);
+    const [message, setMessage] = useState("");
+
+    function connectWallet() {
+        if (ethereum) {
+            ethereum
+                .request({
+                    method: "eth_requestAccounts",
+                })
+                .then(() => {
+                    const provider = new ethers.providers.Web3Provider(ethereum);
+                    setSigner(provider.getSigner());
+                    provider
+                        .getSigner()
+                        .getAddress()
+                        .then((address) => {
+                            setAddress(address);
+                        });
+                    console.log(provider);
+                })
+                .catch(() => {
+                    console.log("Failed to connect to MetaMask");
+                });
+        }
+    }
+    async function deployContract() {
+        try {
+            await ethereum.request({
+                method: "wallet_switchEthereumChain",
+                params: [{ chainId: "0x4" }],
+            });
+        } catch (switchError) {
+            // This error code indicates that the chain has not been added to MetaMask.
+            if (switchError.code === 4902) {
+                try {
+                    await ethereum.request({
+                        method: "wallet_addEthereumChain",
+                        params: [
+                            {
+                                chainId: "0x4",
+                                chainName: "Rinkeby",
+                                rpcUrls: ["https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"],
+                            },
+                        ],
+                    });
+                } catch (addError) {
+                    // handle "add" error
+                }
+            }
+            // handle other "switch" errors
+        }
+        const contractJson = require("./contracts/Greeter.json");
+        // create contract factory
+        const factory = new ethers.ContractFactory(contractJson.abi, contractJson.bytecode, signer);
+        // create contract
+        try {
+            factory.deploy("Hello World").then(async (contract) => {
+                setMessage("Deploying contract...");
+                await contract.deployed();
+                setContract(contract);
+                setMessage("Contract deployed!");
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function callContract() {
+        alert(await contract.greet());
+    }
+
+    return (
+        <div className="App">
+            <div style={{ marginTop: 50 }}>
+                {signer ? (
+                    <div>
+                        <div>{address}</div>
+                        {contract ? (
+                            <div>
+                                <button
+                                    type="button"
+                                    className="btn btn-primary"
+                                    style={{ marginTop: 50 }}
+                                    onClick={callContract}
+                                >
+                                    Call Contract
+                                </button>
+                            </div>
+                        ) : (
+                            <div>
+                                <button
+                                    type="button"
+                                    className="btn btn-primary"
+                                    style={{ marginTop: 50 }}
+                                    onClick={deployContract}
+                                >
+                                    Deploy Contract
+                                </button>
+                            </div>
+                        )}
+                        <div>{message}</div>
+                    </div>
+                ) : (
+                    <button type="button" className="btn btn-primary" onClick={connectWallet}>
+                        Connect Wallet
+                    </button>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
